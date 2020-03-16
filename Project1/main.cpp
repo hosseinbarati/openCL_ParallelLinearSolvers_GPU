3a9ace8bd60111835df726dc206822fe642eac87
Hossein Barati <hossein.barati72@gmail.com>
2020-03-16 12:00:27 +0330

    edit host programming

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 
 
-#include"globals.h"
+#include"openCL_Host.h"
 
 
 
@@ -12,13 +12,20 @@ int main() {
 
 	register cl_int i, j;
 	cl_int pidx, didx;
+	cl_device_type devtype = CL_DEVICE_TYPE_GPU;
+	//cl_device_type devtype = CL_DEVICE_TYPE_GPU || CL_DEVICE_TYPE_ACCELERATOR;
 	std::string platformName, deviceName;
-	SELECT_PLATFORMS_AND_DEVICES(pidx, didx);
+	SELECT_PLATFORMS_AND_DEVICES(pidx, didx, devtype);
 	cl::Context context1;
-	context1 = createContextFromIndex(pidx, didx, CL_DEVICE_TYPE_GPU);
+	context1 = createContextFromIndex(pidx, didx, devtype);
 	ContextDevice(context1);
-	
-	
+	try {
+		cl::CommandQueue queue = cl::CommandQueue(context1, context1.getInfo<CL_CONTEXT_DEVICES>()[0], CL_QUEUE_PROFILING_ENABLE);
+	} catch (cl::Error &e) {
+		std::cout << "Error in function " << e.what() << ": " << e.err() << std::endl;
+	}
+
+
 
 
 	cl_char slp;
